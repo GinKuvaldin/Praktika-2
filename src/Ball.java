749ec960d08a5283fbdcffd672fce8dc15a3905a
ivc6-10 <lyamin-2001@mail.ru>
2020-09-03 12:00:26 +0300

    Инициализация проекта

---
+++ src/Ball.java
@@ -0,0 +1,19 @@
+public class Ball
+{
+    public static void main {String[] arg}
+
+    {
+        Ball a = new Ball();
+        a.color = "Blue";
+        a.radius = 5.47f;
+        a.type = "Football";
+        Ball b = new Ball();
+        b.color = "Red";
+        b.radius = 6.1f
+        b.type="Basketball";
+        System.out.println(b);
+        System.out.println();
+        System.out.println(a);
+
+    }
+}
